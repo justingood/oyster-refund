00598c343c37ec929436839fdca6c6faeb848b34
Justin Good <justingood@gmail.com>
2017-11-15 23:25:59 +0000

    making progress with beautifulsoup

--- getlist.py
+++ getlist.py
@@ -1,10 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
+import yaml
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'UserName': 'user here', 'Password': 'password here'}
+payload = {'UserName': cfg['username'], 'Password': cfg['password']}
 session = requests.Session()
 
 r = session.post('https://account.tfl.gov.uk/Login', headers=headers, data=payload)
+soup = BeautifulSoup(r.content, "html.parser")
+cardlink = soup.find('a', {'data-pageobject': 'mycards-card-cardlink'})
+print(type(cardlink))
 
 # r = session.get('https://oyster.tfl.gov.uk/oyster/journeyHistory.do')
-print(r.content)
+# print(r.content)
